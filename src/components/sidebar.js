1ece5b883efa3839eff43a9990fc2b5feaf13134
nishadnp <69951135+nishadnp@users.noreply.github.com>
2026-03-31 14:40:10 +0400

    feat: add functional sidebar
    - Added a sidebar component with highlight-cards for different weather
      parameters (humidity, UV index, visibility, wind speed).
    - Added new icons for the weather parameters in the assets folder.
    - Updated index.html to include the sidebar and main content area.
    - Styled the sidebar and highlight-cards in layout.css for a clean and
      responsive design.
    - Temporary border for the highlight-cards for visualization and
      background color, to be removed later.
    - Renamed site-logo.png.
    - weather-content is added as placeholder for the main weather information
      to be displayed in the future.

---
+++ src/components/sidebar.js
@@ -0,0 +1,30 @@
+// src/components/sidebar.js
+
+export function renderSideBar(processedData) {
+  // Keys for the weather parameters to be displayed in the highlight cards
+  const keys = ["windSpeed", "humidity", "uvIndex", "visibility"];
+
+  // Units for the different weather parameters
+  const units = {
+    windSpeed: "mph",
+    humidity: "%",
+    uvIndex: "UV",
+    visibility: "miles",
+  };
+
+  const highlightCards = document.querySelectorAll(".highlight-card");
+
+  // Loop through the highlight cards and populate them with the corresponding weather data
+  highlightCards.forEach((highlightCard, index) => {
+    // Clear existing data in the card before adding new data
+
+    let text = highlightCard.querySelector("p");
+    if (!text) {
+      text = document.createElement("p");
+      highlightCard.appendChild(text);
+      text.style.fontSize = "1.5rem";
+    }
+
+    text.textContent = processedData[keys[index]] + " " + units[keys[index]];
+  });
+}
--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import "./styles/layout.css";
 import "./styles/components.css";
 import { getWeather } from "./api/weather";
 import { processWeatherData } from "./utils/processWeatherData";
+import { renderSideBar } from "./components/sidebar";
 
 // Event listener for the weather form submission
 document.getElementById("weather-form").addEventListener("submit", (event) => {
   event.preventDefault();
   const cityInput = document.getElementById("cityInput");
   const city = cityInput.value;
-
   // Fetch and process weather data for the entered city
   getWeather(city)
     .then(processWeatherData)
     .then((processedData) => {
       console.log(`Selected weather API data for ${city}: `, processedData);
+      renderSideBar(processedData);
     })
     .catch((error) => {
       console.error("Error fetching or processing weather data:", error);
