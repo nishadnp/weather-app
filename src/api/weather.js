b4ffd259c414e0a6a22366087c4ab379bf58875e
nishadnp <69951135+nishadnp@users.noreply.github.com>
2026-04-10 02:58:42 +0400

    feat: add 7-day API date range helper function

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,5 +1,7 @@
 // src/api/weather.js
 
+import { format, subDays, addDays } from "date-fns";
+
 // API config (constants only), freeze prevents accidental changes to config values at runtime
 const API_CONFIG = Object.freeze({
   baseURL:
@@ -15,14 +17,27 @@ export function getWeather(locationQuery) {
   // trim + encode ensures safe URL input (e.g. "New York" works)
   const safeLocation = encodeURIComponent(locationQuery.trim());
 
+  const dateRange = get7DayRangeFromYesterday();
+
   console.log("Fetching weather data...");
 
   return fetch(
-    `${API_CONFIG.baseURL}/${safeLocation}?unitGroup=${API_CONFIG.defaultUnit}&key=${API_CONFIG.apiKey}&contentType=json`
+    `${API_CONFIG.baseURL}/${safeLocation}/${dateRange}?unitGroup=${API_CONFIG.defaultUnit}&key=${API_CONFIG.apiKey}&contentType=json`
   ).then((response) => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     return response.json();
   });
+}
+
+// Builds API date range string for 7 days window (yesterday to +5 days from today); Format: yyyy-MM-dd/yyyy-MM-dd
+function get7DayRangeFromYesterday() {
+  const startDateObj = subDays(new Date(), 1); // yesterday
+  const endDateObj = addDays(startDateObj, 6);
+
+  const startDate = format(startDateObj, "yyyy-MM-dd");
+  const endDate = format(endDateObj, "yyyy-MM-dd");
+
+  return `${startDate}/${endDate}`;
 }
--- src/utils/processWeatherData.js
+++ src/utils/processWeatherData.js
@@ -2,14 +2,15 @@
 
 // Function to process the weather data received from the API
 export function processWeatherData(data) {
+  console.log(data);
   // This function can be expanded to process the weather data as needed
   console.log("Processing weather data...");
 
   const currentConditions = data.currentConditions; // current weather conditions
-  const today = data.days[0]; // today’s forecast
+  const today = data.days[1]; // today’s forecast
 
   // Process the next five days' brief forecast
-  const nextFiveDays = data.days.slice(1, 6).map((day) => ({
+  const nextFiveDays = data.days.slice(2, 7).map((day) => ({
     dayName: new Date(day.datetime).toLocaleDateString("en-US", {
       weekday: "long",
     }),
